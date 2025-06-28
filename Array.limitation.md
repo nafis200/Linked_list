2c64907f135e6e52de42c62cb2e76714ad3b17ae
nafis200 <nafisahamed14@gmail.com>
2025-06-28 22:39:08 +0600

    first commit

---
+++ Array.limitation.md
@@ -0,0 +1,4 @@
+# Array limitation.
+
+1. Fixed Size.
+2. Sequential Memory Allocation.
---
+++ byte.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    char b;
+    cout << (void*)&b << " " << (void*)&a << "\n";
+}
