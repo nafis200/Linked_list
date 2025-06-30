49e0a2481d8043054e011eb9604dea24a64ae2bd
nafis200 <nafisahamed14@gmail.com>
2025-06-30 21:23:34 +0600

    newnode

---
+++ Head_linked_list.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// head কে reference দিয়ে পাঠানো হয়েছে
+void insert_at_head(Node* &head, int val){
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+
+    head->next = a;
+
+  
+    insert_at_head(head, 100);
+
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
