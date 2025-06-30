f03b7d59a6d9db0a0b5570b39f8bd939b34e9808
nafis200 <nafisahamed14@gmail.com>
2025-06-30 22:21:32 +0600

    tail

---
+++ Tail_linked_list.cpp
@@ -0,0 +1,89 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int val;
+//     Node *next;
+//     Node(int val)
+//     {
+//         this->val = val;
+//         this->next = NULL;
+//     }
+// };
+
+// void insert_at_head(Node* head, int val){
+//     Node* newNode = new Node(100);
+//     newNode->next = head;
+//     head = newNode;
+// }
+
+// int main()
+// {
+//     Node *head = new Node(10);
+//     Node *a = new Node(20);
+
+//     head->next = a;
+    
+//      insert_at_head(head, 100);
+  
+//     Node *temp = head;
+
+//     while (temp != NULL)
+//     {
+//         cout << temp->val << " ";
+//         temp = temp->next;
+//     }
+// } 
+
+// its wrong why 
+
+
+// but its correct
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+
+void insert_at_tail(Node* &head, int val){
+    Node* newNode = new Node(val);
+    
+    Node *tmp = head;
+    while(tmp->next != NULL){
+           tmp = tmp->next;
+    }
+    tmp->next = newNode;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+
+    head->next = a;
+
+    insert_at_tail(head, 40);
+
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
