f3b15baf4e68d1e9c8ce92c56f49249a4d6a5a5f
nafis200 <nafisahamed14@gmail.com>
2025-07-01 23:21:44 +0600

    as

---
+++ Delete_at_any_position.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    tail->next = newNode;
+    tail = tail->next;
+}
+
+void print(Node *head)
+{
+    if(head == NULL){
+        return;
+    }
+    print(head->next);
+    cout << head->val <<"\n";
+    return;
+}
+
+void delete_node(Node* head, int val){
+    
+    Node* tmp = head;
+
+    for(int i = 1; i < val; i++){
+        tmp = tmp->next;
+    }
+
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteNode;
+    
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *temp = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, temp, val);
+    }
+
+    delete_node(head,2);
+
+    print(head);
+
+    return 0;
+}
